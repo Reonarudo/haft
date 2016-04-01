98b8bd5a624b77a786bbfda14e0741f06d26631b
Leonardo Marques <Leonardo.Marques@t-systems-mms.com>
2016-04-01 13:39:42 +0200

    translated fib and array write to RUST

---
+++ src/benches/simple/arraywrite/src/arraywrite.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::char;
+
+const MAXSIZE:usize = 1000*1000;
+
+static mut arr:[char;MAXSIZE]=[' ';MAXSIZE];
+
+fn arraywrite(size:usize){
+    for i in (0..size).rev(){
+		unsafe{
+		//TODO: check what happens when usize is 64 bit
+			arr[i]=char::from_digit(i as u32, 10).unwrap();
+		}
+	}
+
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: {} char_array_size\n", args[0]);
+	}else{
+		    let size:usize=args[1].parse::<usize>().unwrap();
+    
+		    arraywrite(size);
+			unsafe{
+				//TODO:Don't understand why print arr[0]
+				println!("arr[0]:{} \n",arr[0]);
+			}
+	}
+}
